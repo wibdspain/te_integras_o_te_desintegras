d7817329ecf48b3e8ccce210eabca92e7f17b4b3
estelabalboa <estela.balboa@gmail.com>
2020-02-14 23:17:25 +0100

    feat: se añade segundo ejemplo en pyspark

--- src/main/app/0_intro_production_step.py
+++ src/main/app/0_intro_production_step.py
@@ -1,3 +1,7 @@
+from typing import List, Tuple
+
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
 from main.utils.utils_demo import init_spark
 
 
@@ -12,11 +16,31 @@ def check_prime_num(number: int):
         return print(input, "No es un numero primo")
 
 
+def creating_data(data: list):
+    store_schema = StructType([
+        StructField('date', StringType(), True),
+        StructField('time', StringType(), True),
+        StructField('transaction', IntegerType(), True),
+        StructField('item', StringType(), True)
+    ])
+    store_records_df = spark.createDataFrame(data, store_schema)
+
+    return store_records_df.show()
+
+
 if __name__ == '__main__':
 
     spark = init_spark("testing_environments")
 
-    print(7, "es numero primo?", check_prime_num(7))
-    print("FIN PROCESO \n")
+    print("Loading first function:")
+    number = 7
+    print(number, "is a prime number?", check_prime_num(number))
+    print("End of first function \n")
+
+    print("Loading second function:")
+    data = [('2020-02-20', '10:13:27', 2, 'Jeans'),
+            ('2020-02-18', '11:45:43', 1, 'Sun_glasses')]
+    print("Store clothes sold are:", creating_data(data))
+    print("End of second function \n")
 
     spark.stop()
